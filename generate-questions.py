063e00d470ce0987c702ff73e69c95d57cd9a80c
Scott Sievert <me@scottsievert.com>
2016-06-23 14:50:24 -0500

    generates questions based off what we think is important

--- generate-questions.py
+++ generate-questions.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 from pprint import pprint
 
+
 def find_properties(n):
     props = []
     if n in {1, 3, 5, 7, 11, 13}:
@@ -9,10 +10,15 @@ def find_properties(n):
     for mul in [2, 3, 4, 5]:
         if n % mul == 0:
             props += ['{} mult'.format(mul)]
+    if n in {1, 4, 9}:
+        props += ['square']
     return props
+
 
 def find_numbers_with_prop(prop, props):
     return {i for i in props if prop in props[i]}
+
+
 def find_numbers_without_prop(prop, props):
     return {i for i in props if prop not in props[i]}
 
@@ -20,12 +26,10 @@ props = {n: find_properties(n) for n in 1 + np.arange(12)}
 
 possible_queries = {}
 for a in props:
-    possible_queries[a] = []
     for prop_to_test in props[a]:
         pos_n = find_numbers_with_prop(prop_to_test, props) - {a}
         neg_n = find_numbers_without_prop(prop_to_test, props) - {a}
         for b, c in zip(pos_n, neg_n):
-            possible_queries[a] += [(a, b, c)]
+            possible_queries = {*possible_queries, (a, b, c)}
 
-# for n in 1 + np.arange(12):
-
+print(possible_queries)
