617f9c5f2de8c88c5627f014fd3e0a404e461c7d
Scott Sievert <me@scottsievert.com>
2016-07-06 16:56:57 -0500

    ENH: generates experiment, ready for deployment

--- next/lucid_brain.py
+++ next/lucid_brain.py
@@ -4,20 +4,16 @@ Usage:
     launch_lucid_brain.py scp
 """
 from __future__ import print_function
-import numpy
-import numpy as np
-import numpy.random
-import random
 import json
 import time
-from datetime import datetime
 import requests
-from scipy.linalg import norm
-import time
 from multiprocessing import Pool
 from docopt import docopt
-import sys
 import os
+import pickle
+import numpy as np
+np.random.seed(42)
+
 
 def make_targetset():
     return [{'target_id': str(i),
@@ -27,8 +23,20 @@ def make_targetset():
              'alt_description': str(i + 1)} for i in range(12)]
 
 
-HOSTNAME = os.environ.get('NEXT_BACKEND_GLOBAL_HOST', 'localhost')
-host_file = os.environ.get('KEY_FILE')
+def generate_query(list_):
+    q = np.random.randint(12, size=(3,))
+    while (q.tolist() in list_ or q[0] == q[1] or q[1] == q[2] or
+           q[0] == q[-1]):
+        q = np.random.randint(12, size=(3,))
+    return q.tolist()
+
+
+def generate_query_list(n):
+    query_list = []
+    for n in range(n):
+        query_list += [generate_query(query_list)]
+    return query_list
+
 
 def run_all(assert_200, alg):
     # num_objects = 5
@@ -52,11 +60,7 @@ def run_all(assert_200, alg):
         alg_item['alg_id'] = alg_id
         if alg_id == 'ValidationSampling':
             alg_item['alg_label'] = 'Test'
-            alg_item['params'] = {'query_list': [
-                [q1, q2, q3] for q1 in [0, 1, 2]
-                             for q2 in [0, 1, 2]
-                             for q3 in [0, 1, 2]
-                                             ]}
+            alg_item['params'] = {'query_list': generate_query_list(12)}
         else:
             alg_item['alg_label'] = alg_id
         alg_item['test_alg_label'] = 'Test'
@@ -83,39 +87,48 @@ def run_all(assert_200, alg):
     initExp_args_dict['args']['alg_list'] = alg_list #optional field
     initExp_args_dict['args']['instructions'] = 'You want instructions, here are your test instructions'
     initExp_args_dict['args']['debrief'] = 'You want a debrief, here is your test debrief'
+    initExp_args_dict['args']['num_tries'] = 200
     initExp_args_dict['args']['targets'] = {}
     initExp_args_dict['args']['targets']['targetset'] = make_targetset()
 
     exp_info = []
     for ell in range(num_experiments):
-      url = "http://"+HOSTNAME+":8000/api/experiment"
-      response = requests.post(url, json.dumps(initExp_args_dict), headers={'content-type':'application/json'})
-      print("POST initExp response =",response.text, response.status_code)
-      if assert_200: assert response.status_code is 200
-      initExp_response_dict = json.loads(response.text)
+        url = "http://"+HOSTNAME+":8000/api/experiment"
+        response = requests.post(url,
+                                 json.dumps(initExp_args_dict),
+                                            headers={'content-type': 'application/json'})
+        print("POST initExp response =", response.text, response.status_code)
+        if assert_200:
+            assert response.status_code is 200
+        initExp_response_dict = json.loads(response.text)
 
-      exp_uid = initExp_response_dict['exp_uid']
+        exp_uid = initExp_response_dict['exp_uid']
 
-      exp_info.append( {'exp_uid':exp_uid,} )
+        exp_info.append({'exp_uid': exp_uid})
 
-      #################################################
-      # Test GET Experiment
-      #################################################
-      print('\n'*2 + 'Testing GET initExp...')
-      url = "http://"+HOSTNAME+":8000/api/experiment/"+exp_uid
-      response = requests.get(url)
-      print("GET experiment response =",response.text, response.status_code)
-      if assert_200: assert response.status_code is 200
-      initExp_response_dict = json.loads(response.text)
+        #################################################
+        # Test GET Experiment
+        #################################################
+        print('\n'*2 + 'Testing GET initExp...')
+        url = "http://"+HOSTNAME+":8000/api/experiment/"+exp_uid
+        response = requests.get(url)
+        print("GET experiment response =", response.text, response.status_code)
+        if assert_200:
+            assert response.status_code is 200
+        initExp_response_dict = json.loads(response.text)
 
-      print('\nQuery URL available at http://{}:8000/query/query_page/query_page/{}\n'.format(HOSTNAME, exp_uid))
-      return exp_uid
+        print('\nQuery URL available at '
+              'http://{}:8000/query/query_page/'.format(HOSTNAME) +
+              'query_page/{}\n'.format(exp_uid))
+        return exp_uid
 
 
 
 def timeit(f):
   """ 
-  Utility used to time the duration of code execution. This script can be composed with any other script.
+
+  Utility used to time the duration of code execution. This script can be
+  composed with any other script.
 
   Usage::\n
     def f(n): 
@@ -137,19 +150,53 @@ def timeit(f):
       return result,(te-ts)
   return timed
 
-if __name__ == '__main__':
-  print(HOSTNAME)
-  args = docopt(__doc__, version='NEXT')
-  if args['launch']:
-      exp_uids = {}
-      for alg in ['CrowdKernel', 'ValidationSampling']:
-          exp_uid = run_all(True, alg)
-          exp_uids[alg] = exp_uid
-      print(exp_uids)
+def put_exp_uids_in(name, exp_uids):
+    out_file = open(name + '_out.html', 'wb')
+    with open(name + '.html', 'rb') as f:
+        for line in f.readlines():
+            process = False
+            if '# random list' in line:
+                random_list = np.random.randint(4, 190, size=(20,))
+                random_list = np.sort(random_list).tolist()
+                line = """ 
+          var validation_queries = {};
+""".format(random_list)
+            if 'ValidationSampling' in line:
+                exp_uid = '{}'.format(exp_uids['ValidationSampling'])
+                process = True
+            if 'UncertaintySampling' in line:
+                exp_uid = '{}'.format(exp_uids['UncertaintySampling'])
+                process = True
+            if process:
+                line = ' '*15 + 'exp_uid = "{}";\n'.format(exp_uid)
+                del exp_uid
+            print(line, file=out_file, end='')
 
-  if args['scp']:
-      os.system('scp -i "{}" query_page_choose.html ubuntu@{}:'.format(host_file, HOSTNAME) +
-              '/usr/local/next-discovery/next/query_page/templates/query_page_choose.html')
-      # to undo this scp, rsync from the main next folder
-      os.system('scp -i "{}" getQuery_widget_delay.html ubuntu@{}:'.format(host_file, HOSTNAME) +
-              '/usr/local/next-discovery/next/apps/Apps/PoolBasedTripletMDS/widgets/getQuery_widget.html')
+if __name__ == '__main__':
+    HOSTNAME = os.environ.get('NEXT_BACKEND_GLOBAL_HOST', 'localhost')
+    host_file = os.environ.get('KEY_FILE')
+    print(HOSTNAME)
+    args = docopt(__doc__, version='NEXT')
+    exp_uid_filename = 'exp_uids.pkl'
+    if args['launch']:
+        exp_uids = {}
+        for alg in ['UncertaintySampling', 'ValidationSampling']:
+            exp_uid = run_all(True, alg)
+            exp_uids[alg] = exp_uid
+        print(exp_uids)
+        pickle.dump(exp_uids, open(exp_uid_filename, 'wb'))
+
+    if args['scp']:
+        exp_uids = pickle.load(open(exp_uid_filename, 'rb'))
+        put_exp_uids_in('query_page_choose', exp_uids)
+
+        # to undo this scp, rsync from the main next folder
+        os.system('scp -i "{}" getQuery_widget_delay.html ubuntu@{}:'.format(host_file, HOSTNAME) +
+                '/usr/local/next-discovery/next/apps/Apps/PoolBasedTripletMDS/widgets/getQuery_widget.html')
+
+        os.system('scp -i "{}" query_page_choose_out.html ubuntu@{}:'.format(
+                  host_file, HOSTNAME) +
+                  '/usr/local/next-discovery/next/query_page/templates/query_page_choose.html')
+
+        print('\nQuery URL:\nhttp://{}:8000/query/query_page/query_page_choose/{}'
+              .format(HOSTNAME, exp_uids['UncertaintySampling']))
